828b430968fe48645f7bd9a885de976a6e9e4b67
KangJunWoo <jwkang5665@gmail.com>
2021-08-13 18:27:04 +0900

    ..

--- src/main/java/com/project/jun/controller/OauthController.java
+++ src/main/java/com/project/jun/controller/OauthController.java
@@ -1,5 +1,6 @@
 package com.project.jun.controller;
 
+import java.io.IOException;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
@@ -18,7 +19,6 @@ import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.project.jun.domain.UserInfo;
 import com.project.jun.service.UserService;
-import com.project.jun.springsecurity.CustomAuthenticationManager;
 
 
 @RestController
@@ -30,14 +30,13 @@ public class OauthController {
 	
 	
 	@RequestMapping(value="/getMyServerToken", method=RequestMethod.POST)
-	public String getMyServerToken(HttpServletResponse response ,@RequestBody Map<String, String> map) {
+	public String getMyServerToken(HttpServletResponse response ,@RequestBody Map<String, String> map) throws IOException {
 		System.out.println("getMyServerToken시작");
 		System.out.println(map);
 		System.out.println(map.get("email"));
 		String email=map.get("email");
 		List<UserInfo> userInfos=userService.selectAllUserInfo();
 		System.out.println("Kakao사용자 정보에서 받은 email과 DB의 ID와 비교 후 일치하면 Token생성");
-		System.out.println(userInfos.get(2).getUser_id());
 		for (UserInfo userInfo : userInfos) {
 			if (userInfo.getUser_id().equals(email)) {
 				System.out.println("=======================ID일치=======================");
@@ -48,11 +47,13 @@ public class OauthController {
 						.withClaim("username", userInfo.getUsername())
 						.sign(Algorithm.HMAC512("juns"));
 				response.addHeader("Authorization", "Bearer "+jwtToken);
+				return "";
 			}
 		}
 		System.out.println("=======================ID 불일치=======================");
 		System.out.println("=======================사용자 정보 email을 통하여 DB에 ID insert=======================");
+		userService.inserUserInfo(new UserInfo(0, email, "1234", "USER", null));
+		return "";
 		
-		return "asdfasdfasdfsda";
 	}
 }
